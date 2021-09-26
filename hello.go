730fd03f8e4da4ad2efdf19f9a18980e61222a66
alexzshl <alexzshl@126.com>
2021-09-26 19:32:18 +0800

    update 222

--- hello.go
+++ hello.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("hello drone")
 	fmt.Println("aaa")
+
+	fmt.Println()
+
+	aSecret := os.Getenv("A_SECRET")
+	fmt.Println(aSecret)
+	fmt.Println(reverseString(aSecret))
+}
+
+func reverseString(s string) string {
+	r := []rune(s)
+	sb := strings.Builder{}
+	for i := len(r) - 1; i >= 0; i-- {
+		sb.WriteRune(r[i])
+	}
+	return sb.String()
 }
